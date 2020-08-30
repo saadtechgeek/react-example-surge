49a7fddf9899a48e02e1c8e15901e717c30e4144
Saad Shahid <saad.techgeek@gmail.com>
2020-08-30 17:00:33 +0500

    change in index file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Hello World And Thanks Saad</h2>
+          <h2>Hello World</h2>
         </div>
       </div>
     );
